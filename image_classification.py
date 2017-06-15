42f7f535531fba7ec233bd2eb62d6e67eed673b2
Yassine <yassinealouini@outlook.com>
2017-06-15 09:27:00 +0100

    Add smaller cat images. Add label to the image

--- image_classification.py
+++ image_classification.py
@@ -1,3 +1,6 @@
+# Inspired from this blog post:
+# http://www.pyimagesearch.com/2016/08/10/imagenet-classification-with-python-and-keras/
+
 import argparse
 
 import numpy as np
@@ -8,20 +11,28 @@ from keras.applications.imagenet_utils import (decode_predictions,
                                                preprocess_input)
 from keras.applications.resnet50 import ResNet50
 
+TEXT_COLOR = (255, 255, 255)
+
 if __name__ == '__main__':
 
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-i", "--image", required=True,
-                    help="path to the input image")
-    args = vars(ap.parse_args())
-    # load the original image via OpenCV so we can draw on it and display
-    # it to our screen later
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--image", required=True,
+                        help="path to the input image")
+    args = vars(parser.parse_args())
     orig = cv2.imread(args["image"])
+    # Load and process the image
     image = image_utils.load_img(args["image"], target_size=(224, 224))
     image = image_utils.img_to_array(image)
     image = np.expand_dims(image, axis=0)
     image = preprocess_input(image)
+    # Load the model and make predictions
     model = ResNet50()
     preds = model.predict(image)
+    print('The top 5 labels are: (in descending order)')
     for inID, label, score in decode_predictions(preds)[0]:
         print(label, score)
+    top_label = decode_predictions(preds)[0][0][1]
+    cv2.putText(orig, "Top label: {}".format(top_label), (10, 30),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.9, TEXT_COLOR, 2)
+    cv2.imshow("Classification", orig)
+    cv2.waitKey(0)
