0098a0005bde0f53bd54a75dac3cd0ffca7ba530
Yassine <yassinealouini@outlook.com>
2017-12-25 10:35:59 +0100

    Add a test GET route

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
+import os
+
 from flask import Flask
 
-# from image_classification import ImageClassifier
+import keras.preprocessing.image as image_utils
+from image_classification import ImageClassifier
 
 app = Flask(__name__)
-PORT = 33507
-HOST = '0.0.0.0'
 
 
 @app.route('/')
@@ -12,5 +13,18 @@ def home():
     return 'Hello classification world!'
 
 
+@app.route('/test')
+def test():
+    """ Test that the image recognition model is working using a local image.
+    """
+    test_path = os.path.join(os.path.dirname(
+        os.path.realpath(__file__)), 'data', 'cat_awake.JPG')
+    img_clf = ImageClassifier()
+    top_label = img_clf.get_top_label(test_path)
+    return top_label
+
+
 if __name__ == '__main__':
+    PORT = int(os.environ.get('PORT', 5000))
+    HOST = '0.0.0.0'
     app.run(host=HOST, port=PORT)
