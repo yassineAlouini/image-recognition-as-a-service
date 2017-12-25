e3e1115286859f94bc81197a8abb4368b3a66703
Yassine <yassinealouini@outlook.com>
2017-12-25 16:47:36 +0100

    Some clean-up of the API layer

--- app.py
+++ app.py
@@ -6,17 +6,18 @@ from werkzeug.utils import secure_filename
 import keras.preprocessing.image as image_utils
 from image_classification import ImageClassifier
 
-app = Flask(__name__)
 DATA_FOLDER_PATH = os.path.join(
     os.path.dirname(os.path.realpath(__file__)), 'data')
+
+app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = DATA_FOLDER_PATH
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 img_clf = ImageClassifier()
 
 
 @app.route('/upload', methods=['GET', 'POST'])
-def upload_and_rec_img():
-    """ Upload and recognize an image.
+def upload_img():
+    """ Upload an image and redirect to the recognition URL.
     """
     # Inspired from this:
     # http://flask.pocoo.org/docs/0.12/patterns/fileuploads/#uploading-files
@@ -33,28 +34,15 @@ def upload_and_rec_img():
 
 @app.route('/upload/<filename>')
 def rec_img(filename):
+    """ Use the image classification class to recognize the top label on the
+    uploaded image.
+    """
     try:
         img_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         top_label = img_clf.get_top_label(img_path)
-        # Remove the image to avoid flooding the disk
-        os.remove(img_path)
         return f"The top label for the  uploaded image is: {top_label}"
     except FileNotFoundError:
         return "The requested image wasn't found. :("
-
-
-@app.route('/')
-def home():
-    return 'Welcome to your image recognition API!'
-
-
-@app.route('/test')
-def test():
-    """ Test that the image recognition model is working using a local image.
-    """
-    test_path = os.path.join(DATA_FOLDER_PATH, 'cat_awake.JPG')
-    top_label = img_clf.get_top_label(test_path)
-    return f"The top label for the test image is: {top_label}"
 
 
 if __name__ == '__main__':
